67f7eb482211bad2ae09495a4f7a934df3c39c2f
Akash <akash7nair@gmail.com>
2021-10-17 20:10:21 -0400

    added issue and label

--- issueLabels.py
+++ issueLabels.py
@@ -13,31 +13,31 @@ def listIssueNumber(repo):
 	return label 
 
 
-def listLabels(repo, csv):
+def listLabels(repo, iss):
 	if(repo.strip() == ''):
 		return "";
 	labelSet = set() 
-	for iss in csv:
-		response = requests.get("https://api.github.com/repos"+repo+"issues/"+str(iss)+"/labels", headers={"Authorization":"token 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"})
-		data = json.loads(response.text)
-		if(len(data)==0):
-			return labelSet 
-		for name in data:
-			#issueName.append(name["name"])
-			labelSet.add(name["name"])
-	print(labelSet)
+	response = requests.get("https://api.github.com/repos"+repo+"issues/"+str(iss)+"/labels", headers={"Authorization":"token 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"})
+	data = json.loads(response.text)
+	if(len(data)==0):
+		return labelSet 
+	for name in data:
+		#issueName.append(name["name"])
+		labelSet.add(name["name"])
 	return labelSet;
 
 def main():
 	file = open('repo3','r')	
-	topology_list = file.read().splitlines()
+	repo_list= file.read().splitlines()
 	f = open("labelout", "w")
-	for i in topology_list:
-		csv =listIssueNumber(i)
-		label=str(listLabels(i,csv))
-		print(label)
-		print(i+"\t"+str(csv)+"\t"+label+"\n")
-		f.write(i+"\t"+str(csv)+"\t"+label+"\n")
+	for repo in repo_list:
+		csv =listIssueNumber(repo)
+		print(repo)
+		f.write(repo+"\n")
+		for iss in csv:
+			label = str(listLabels(repo,iss))
+			print(str(iss)+"\t"+label+"\n")
+			f.write(str(iss)+"\t"+label+"\n")
 	f.close();
 if __name__=="__main__":
 	main()
